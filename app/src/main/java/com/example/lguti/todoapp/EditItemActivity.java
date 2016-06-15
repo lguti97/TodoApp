25cbf4818910430f92e49ebeeae09fd7b2606e90
Luis Gutierrez <lguti@fb.com>
2016-06-14 19:18:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lguti/todoapp/EditItemActivity.java
@@ -0,0 +1,20 @@
+package com.example.lguti.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        // Find the edit text
+        // Put the text from the extra into edit text
+
+    }
+
+
+}
